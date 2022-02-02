741aa45c5a825a7d91f7e94e564c6fd61b190869
azmul-ml <azmul.hossain@.monstar-lab.com>
2022-02-02 21:56:48 +0600

    google analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
 import "../assets/scss/style.scss";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import type { AppProps } from "next/app";
-import NProgress from 'nprogress';
+import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import { store, persistor } from "../redux/store";
 import { ClearCacheProvider } from "react-clear-cache";
@@ -10,55 +10,63 @@ import "react-multi-carousel/lib/styles.css";
 import { Provider } from "react-redux";
 import Layout from "../layouts/Layout";
 import { ToastProvider } from "react-toast-notifications";
-import { PersistGate } from 'redux-persist/integration/react'
-import Router from 'next/router';
-import { DefaultSeo } from 'next-seo';
-import SEO from '../next-seo.config';
-import * as gtag from '../lib/gtag'
-import {useRouter} from "next/router";
+import { PersistGate } from "redux-persist/integration/react";
+import Router from "next/router";
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config";
+import * as gtag from "../lib/gtag";
+import Script from "next/script";
+import { GA_TRACKING_ID } from "../lib/gtag";
 
-require('isomorphic-fetch');
+require("isomorphic-fetch");
 
 NProgress.configure({
   minimum: 0.5,
-  easing: 'ease',
+  easing: "ease",
   speed: 800,
   showSpinner: true,
-  height: 15
+  height: 15,
 });
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events]);
-
   return (
     <ClearCacheProvider duration={5000}>
-      <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <Script
+        src="https://embed.tawk.to/61ed3eecb9e4e21181bb6ab1/1fq3brjn5"
+        id="ChatTalk"
+        async
+      />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-        <ToastProvider placement="bottom-left">
-          <Layout
-            headerContainerClass="container-fluid"
-            headerPaddingClass="header-padding-1"
-            headerTop="visible"
-          > 
-            <DefaultSeo {...SEO} />
-            <Component {...pageProps} />
-          </Layout>
-        </ToastProvider>
+          gtag('config','${GA_TRACKING_ID}');
+        `}
+      </Script>
+
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ToastProvider placement="bottom-left">
+            <Layout
+              headerContainerClass="container-fluid"
+              headerPaddingClass="header-padding-1"
+              headerTop="visible"
+            >
+              <DefaultSeo {...SEO} />
+              <Component {...pageProps} />
+            </Layout>
+          </ToastProvider>
         </PersistGate>
       </Provider>
     </ClearCacheProvider>
